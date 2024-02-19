2dc34f7fa59d397ec789883ca596ce1db2de4d94
Kostiantyn-Shavrukov <kostiantyn.shavrukov@grammarly.com>
2024-02-18 21:19:29 -0800

    Fixed test run

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,6 @@
 from pyliteway.migrator import PyLiteWay
 
-
-def dummy_test():
+def test_dummy():
     test_val = "ttt"
     pyLiteWay = PyLiteWay(test_val)
     assert pyLiteWay.getPath() == test_val
